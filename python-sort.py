f63a478bba3b1335caed860e0495d880d5dff4df
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2019-05-08 19:30:11 -0700

    Update python-sort.py

--- python-sort.py
+++ python-sort.py
@@ -2,8 +2,17 @@ import random
 import sys
 import string
 
-ram = []*12000
+byteSize = 8
+byteCount = 1500
+
+ram = []*(byteSize * byteCount)
 bitlist = []
+
+def displayAllRecords():
+	i = 1
+	while(i < len(ram)):
+		print(str(i)+":"+str(ram[i]) + " "),
+		i++
 
 def displayRecords(prepend =''):
 	hundredth  = ''.join(str(v) for v in ram[99])
@@ -11,12 +20,8 @@ def displayRecords(prepend =''):
 	print(prepend + 'hundredth element: '  + hundredth)
 	print(prepend + 'thousandth element: ' + thousandth)
 
-def blankOut(byteNumber):
-	ram[byteNumber]='00000000'
-
 def update(byteNumber,newValue):
-	ram[byteNumber]=newValue;
-
+	ram[byteNumber]=newValue
 
 for x in range(12000):
 	bitlist = []
@@ -26,17 +31,6 @@ for x in range(12000):
 	bitstring = ''.join(bitlist)
 	ram.append(bitstring)
 	
-displayRecords()
+displayAllRecords()
 
-#overwrite the 99th and 999th bytes with zeroes
-#blankOut(99)
-#blankOut(999)
 
-displayRecords('overwritten ')
-print('The array can hold a max of 1500 8-byte words for a total size of 12,000 bits.\n')
-
-#print alternate elements
-i = 1
-while(i < len(ram)):
-	print(str(i)+":"+str(ram[i]) + " "),
-	i = i + 2
