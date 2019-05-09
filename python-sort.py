3fa8ae226b0baf3cdcf3699ace28cc5a1c9fbbe1
EC2 Default User <ec2-user@ip-172-31-45-28.ec2.internal>
2019-05-09 02:59:35 +0000

    Created function to find minimum and count the occurances.

--- python-sort.py
+++ python-sort.py
@@ -2,35 +2,74 @@ import random
 import sys
 import string
 
-byteSize = 8
-byteCount = 1500
 
-ram = []*(byteSize * byteCount)
+totalSize = 12000
+totalSize = 11
+ram = []*totalSize
 bitlist = []
 
-def displayAllRecords():
+def displayAllRecords(ofWhat):
 	i = 1
-	while(i < len(ram)):
-		print(str(i)+":"+str(ram[i]) + " "),
+	while(i < len(ofWhat)):
+		print(str(i)+":"+str(ofWhat[i]) + " "),
 		i=i+1
 
-def displayRecords(prepend =''):
-	hundredth  = ''.join(str(v) for v in ram[99])
-	thousandth = ''.join(str(v) for v in ram[999])
-	print(prepend + 'hundredth element: '  + hundredth)
-	print(prepend + 'thousandth element: ' + thousandth)
+def delete(set, byteNumber):
+	set[byteNumber]='00000000'
 
 def update(byteNumber,newValue):
 	ram[byteNumber]=newValue
 
-for x in range(12000):
+for x in range(totalSize):
 	bitlist = []
 	for i in range(0,8):
 		x = str(random.randint(0,1))
 		bitlist.append(x)
 	bitstring = ''.join(bitlist)
 	ram.append(bitstring)
+
+def countValues(input, value):
+	#count number of values in list
+	values=0;
+	i = 1
+	while(i < len(input)):
+		if str(input[i])=='00000000':
+			values = values+1
+		i=i+1
 	
-displayAllRecords()
+	return values
+
+def findBinaryMinimum(input):
+	i = 1
+	min = int(input[i], 2)
+	while(i < len(input)):
+		val = int(input[i], 2)
+		if val < min:
+			min = val
+		i=i+1
+	return min
+
+def selectionSort(input):
+	new=[]*len(input)
+	
+	min = findBinaryMinimum(input)
+	count = countValues(input, min)
+	print('minimum value of ' + str(min) + ' occurred ' + str(count) + ' times')
+	
+	#for x in range(values):
+	#	new.append('000000')
+	
+	return new
+
+
+delete(ram, 8)
+	
+print('Unsorted')
+displayAllRecords(ram)
+
+print('\n')
+print('Selection Sorted')
+selectionSorted = selectionSort(ram)
+displayAllRecords(selectionSorted)
 
 
